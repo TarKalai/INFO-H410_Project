853a2334415aa44c1b3a01f1c6a976c31616a2d2
sebadevo <sebastien.de.vos@ulb.be>
2022-03-05 14:32:41 +0100

    debut d'une grid intéressante

--- game_ui.py
+++ game_ui.py
@@ -3,17 +3,18 @@ import numpy as np
 import colors as c
 import re
 import pathlib
-from tkinter import CENTER
-#from matplotlib.widgets import Widget
+# from tkinter import CENTER
+# from matplotlib.widgets import Widget
 import pygame
 from pygame.locals import *
 
 # Constant for the size of the screen.
 WIDTH = 800
-HEIGHT = 500
+HEIGHT = 510
 # Constant for the size of the text.
 TXT_CORE_SIZE = 38
 TXT_MENU_SIZE = 50
+
 
 class Py2048:
     def __init__(self, gs, screen):
@@ -27,11 +28,10 @@ class Py2048:
         # Attributes for the main core of the game.
         self.screen = screen
         self.gs = gs
-        self.ms = 5
-        self.ts = (HEIGHT-(gs+1)*self.ms)/gs
-        self.grid = np.zeros((gs, gs), dtype=int)
+        self.ms = 6
+        self.ts = (HEIGHT - (gs + 1) * self.ms) / gs
+        self.grid = c.TEST_GRID # np.zeros((gs, gs), dtype=int)
 
-    
     def draw(self):
         # Initialing Color
         self.screen.fill(c.GC['background'])
@@ -39,17 +39,15 @@ class Py2048:
         # Drawing Rectangle
         for x in range(self.gs):
             for y in range(self.gs):
-                n = 2**(self.grid[x][y]+1)
+                n = self.grid[x][y]
                 color = c.GC[n]
-                rect_x = x*(self.ts + self.ms)+self.ms
-                rect_y = y*(self.ts + self.ms)+self.ms
-                pygame.draw.rect(self.screen, color, pygame.Rect(rect_x, rect_y, self.ts, self.ts)) # pos + dimension
-                text_surface=myfont.render(f'{n}', True, (0,0,0)) 
-                text_rect = text_surface.get_rect(center=(rect_x+self.ts/2, rect_y+self.ts/2))
+                rect_x = x * (self.ts + self.ms) + self.ms
+                rect_y = y * (self.ts + self.ms) + self.ms
+                pygame.draw.rect(self.screen, color, pygame.Rect(rect_x, rect_y, self.ts, self.ts))  # pos + dimension
+                text_surface = myfont.render(f'{n}', True, (0, 0, 0))
+                text_rect = text_surface.get_rect(center=(rect_x + self.ts / 2, rect_y + self.ts / 2))
                 self.screen.blit(text_surface, text_rect)
         pygame.display.flip()
-
-
 
         while True:
             for event in pygame.event.get():
@@ -57,9 +55,8 @@ class Py2048:
 
     def add_number(self, nb=1):
         """
-        Will add number 2 or 4 to the grid at a random available position at the begining of each turn. In the beggining
-        of the game we need to add 2 numbers, so we specify nb = 2.
-        :param nb: number of numbers to add
+        Will add number 2 or 4 to the grid at a random available position at the begining of each turn. In the
+        beggining of the game we need to add 2 numbers, so we specify nb = 2. :param nb: number of numbers to add
         :return: /
         """
         available_pos = list(zip(*np.where(self.grid == 0)))
@@ -70,10 +67,6 @@ class Py2048:
             else:
                 self.grid[position] = 4
 
-
-
-
-    
     def exit(self):
         """
         Exit the application.
