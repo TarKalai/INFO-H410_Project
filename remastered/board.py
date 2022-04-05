7979d986b83375152e27939c5a10da7c2ba32e15
Tarik <Tarik.Kalai@ulb.be>
2022-04-05 14:49:18 +0200

    Refactoring

--- remastered/board.py
+++ remastered/board.py
@@ -11,22 +11,16 @@ moves = ['u', 'l', 'r', 'd']
 class Board:
     def __init__(self):
         # get the display surface
+
         self.display_surface = pygame.display.get_surface()
         # sprite group setup
+
         self.visible_sprites = pygame.sprite.Group()
-        self.background_sprites = pygame.sprite.Group()
 
         self.grid = np.zeros((GRIDSIZE, GRIDSIZE), dtype=int)
-        self.tileList = [[0, 0, 0, 0],
-                         [0, 0, 0, 0],
-                         [0, 0, 0, 0],
-                         [0, 0, 0, 0]]
-        self.tileMovement = np.zeros((GRIDSIZE, GRIDSIZE, 2), dtype=int)
-        self.tileToRemove = np.zeros_like(self.grid, dtype=int)
         self.create_board()
-        self.direction = pygame.math.Vector2()
+
         self.canMove = True
-        self.updated = True
         self.score = 0
         self.commands = {
             'u': self.move_up,
@@ -35,168 +29,46 @@ class Board:
             'r': self.move_right
         }
 
+
     def create_board(self):
+        self.add_number(nb=2)
+        self.update_board()
+        
+
+    def update_board(self):
+        self.visible_sprites.empty()
         for row_index, row in enumerate(self.grid):
             for col_index, col in enumerate(row):
                 x = int(col_index * (TILESIZE + 2 * MARGESIZE) + 2 * MARGESIZE)
                 y = int(row_index * (TILESIZE + 2 * MARGESIZE) + 2 * MARGESIZE)
-                Tile((x, y), [self.background_sprites])
-                if self.grid[row_index, col_index]:
-                    self.tileList[row_index][col_index] = Tile((x, y), [self.visible_sprites],
-                                                               self.grid[row_index, col_index])
-        self.add_number(nb=2)
+                Tile((x, y),  [self.visible_sprites], self.grid[row_index, col_index])
+    
 
     def input(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP] or keys[pygame.K_z]:
-            if self.check_valid_move('u'):
-                self.move_up_animation()
-                self.set_tile_target()
-                self.update_tile_list()
-                self.update_tile_value()
-                self.add_number()
-
-        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            if self.check_valid_move('d'):
-                self.move_down_animation()
-                self.set_tile_target()
-                self.update_tile_list()
-                self.update_tile_value()
-                self.add_number()
-
-        elif keys[pygame.K_LEFT] or keys[pygame.K_q]:
-            if self.check_valid_move('l'):
-                self.move_left_animation()
-                self.set_tile_target()
-                self.update_tile_list()
-                self.update_tile_value()
-                self.add_number()
-
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            if self.check_valid_move('r'):
-                self.move_right_animation()
-                self.set_tile_target()
-                self.update_tile_list()
-                self.update_tile_value()
-                self.add_number()
-
-        else:
-            self.tileMovement = np.zeros((GRIDSIZE, GRIDSIZE, 2), dtype=int)
-
-    def set_tile_target(self):
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                if self.tileList[i][j]:
-                    dx, dy = self.tileMovement[i, j]
-                    if dx + dy:
-                        self.tileList[i][j].new_target(self.tileMovement[i, j])
-                        self.tileList[i][j].set_direction(self.tileMovement[i, j])
-
-    def update_tile_list(self):
-        tempList = [[0, 0, 0, 0],
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0]]
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                if self.tileToRemove[i, j]:
-                    self.visible_sprites.remove(self.tileList[i][j])
-                    self.tileList[i][j] = 0
-                elif self.tileList[i][j]:
-                    dx, dy = self.tileMovement[i, j]
-                    if tempList[i + dy][j + dx]:
-                        self.visible_sprites.remove(tempList[i + dy][j + dx])
-                    tempList[i + dy][j + dx] = self.tileList[i][j]
-        self.tileToRemove = np.zeros_like(self.grid, dtype=int)
-        self.tileList = tempList
-
-    def update_tile_value(self):
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                if self.tileList[i][j]:
-                    self.tileList[i][j].update_value(self.grid[i, j])
-
-    def check_deplacement(self):
-        for tile in self.visible_sprites:
-            if not tile.finished_movement():
-                self.canMove = False
-                return
-        self.canMove = True
-
-    def move_up_animation(self):
-        """
-        Will move the tiles upwards and fuse the tiles if they can.
-        """
-        check = np.zeros_like(self.grid, dtype=int)  # a matrice of 0, if the tiles merge, the position of the merge
-        for col in range(GRIDSIZE):  # Lines
-            count = 0
-            for row in range(GRIDSIZE):  # Colones
-                if self.grid[row, col] != 0:
-                    if row > 0 and count > 0 and not check[count - 1, col] and self.grid[count - 1, col] == self.grid[
-                        row, col]:
-                        self.grid[count - 1, col] *= 2
-                        self.tileMovement[row, col] = [0, -(row - count + 1)]
-                        self.grid[row, col] = 0
-                        check[count - 1, col] += 1
-                        self.score += self.grid[count - 1, col]
-                    elif row != count:
-                        self.tileMovement[row, col] = [0, -(row - count)]
-                        self.grid[count, col] = self.grid[row, col]
-                        self.grid[row, col] = 0
-                        count += 1
-                    else:
-                        count += 1
-        self.tileToRemove = check
-
-    def move_right_animation(self):
-        """
-            Will rotate the board matrice to the left, thus making it like a right move is an up move. We can thus apply
-            move_up and then rotate backward the matrice.
-            """
-        self.grid = np.rot90(self.grid, 1)
-        self.tileMovement = np.rot90(self.tileMovement, 1)
-        self.move_up_animation()
-        self.grid = np.rot90(self.grid, -1)
-        self.tileMovement = np.rot90(self.tileMovement, -1)
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                self.tileMovement[i, j, 0] = -self.tileMovement[i, j, 1]
-                self.tileMovement[i, j, 1] = 0
-        self.tileToRemove = np.rot90(self.tileToRemove, -1)
-        print('right')
-
-    def move_left_animation(self):
-        """
-            Will rotate the board matrice to the right, thus making it like a left move is an up move. We can thus apply
-            move_up and then rotate backward the matrice.
-            """
-        self.grid = np.rot90(self.grid, -1)
-        self.tileMovement = np.rot90(self.tileMovement, -1)
-        self.move_up_animation()
-        self.grid = np.rot90(self.grid, 1)
-        self.tileMovement = np.rot90(self.tileMovement, 1)
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                self.tileMovement[i, j, 0] = self.tileMovement[i, j, 1]
-                self.tileMovement[i, j, 1] = 0
-        self.tileToRemove = np.rot90(self.tileToRemove, 1)
-        print('left')
-
-    def move_down_animation(self):
-        """
-            Will rotate the board matrice twice to the left, thus making it like a down move is an up move. We can thus
-            apply move_up and then rotate backward the matrice.
-            """
-        self.grid = np.rot90(self.grid, 2)
-        self.tileMovement = np.rot90(self.tileMovement, 2)
-        self.move_up_animation()
-        self.grid = np.rot90(self.grid, 2)
-        self.tileMovement = np.rot90(self.tileMovement, 2)
-        for i in range(GRIDSIZE):
-            for j in range(GRIDSIZE):
-                self.tileMovement[i, j, 1] = -self.tileMovement[i, j, 1]
-        self.tileToRemove = np.rot90(self.tileToRemove, 2)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_z:
+                        self.move('u')
+                        return 
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.move('r')
+                        return 
+                    elif event.key == pygame.K_LEFT or event.key == pygame.K_q:
+                        self.move('l')
+                        return
+                    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.move('d')
+                        return
+                    elif event.key == pygame.K_ESCAPE:
+                        return 'q' # TODO ecrirela fonction qui appel le menu
+    
+    def move(self, input):
+        print("Je suis dans le move")
+        self.commands[input]()
+        self.add_number()
+        self.update_board()
 
     def add_number(self, nb=1):
         """
@@ -208,31 +80,25 @@ class Board:
         available_pos = list(zip(*np.where(self.grid == 0)))
         if len(available_pos) > 0:
             for position in random.sample(available_pos, k=nb):
-                x = int(position[1] * (TILESIZE + 2 * MARGESIZE) + 2 * MARGESIZE)
-                y = int(position[0] * (TILESIZE + 2 * MARGESIZE) + 2 * MARGESIZE)
                 if random.random() > 0.1:
                     self.grid[position] = 2
                 else:
                     self.grid[position] = 4
-                self.tileList[position[0]][position[1]] = Tile((x, y), [self.visible_sprites], self.grid[position])
+
 
     def run(self):
-        self.background_sprites.draw(self.display_surface)
         self.visible_sprites.draw(self.display_surface)
-        self.check_deplacement()
-        if self.canMove:
-            self.input()
+        self.input()
 
-        self.visible_sprites.update()
 
-    def move_up(self, test_move):
+    def move_up(self, test_move=False):
         """
             Will move the tiles upwards and fuse the tiles if they can.
-            """
+        """
         check = np.zeros_like(self.grid, dtype=int)  # a matrice of 0, if the tiles merge, the position of the merge
         for col in range(GRIDSIZE):  # Lines
             count = 0
-            for row in range(GRIDSIZE):  # Colones
+            for row in range(GRIDSIZE): # Colones
                 if self.grid[row, col] != 0:
                     if row > 0 and count > 0 and not check[count - 1, col] \
                             and self.grid[count - 1, col] == self.grid[row, col]:
@@ -248,7 +114,7 @@ class Board:
                     else:
                         count += 1
 
-    def move_right(self, test_move):
+    def move_right(self, test_move=False):
         """
             Will rotate the board matrice to the left, thus making it like a right move is an up move. We can thus apply
             move_up and then rotate backward the matrice.
@@ -257,7 +123,7 @@ class Board:
         self.move_up(test_move)
         self.grid = np.rot90(self.grid, -1)
 
-    def move_left(self, test_move):
+    def move_left(self, test_move=False):
         """
             Will rotate the board matrice to the right, thus making it like a left move is an up move. We can thus apply
             move_up and then rotate backward the matrice.
@@ -266,7 +132,7 @@ class Board:
         self.move_up(test_move)
         self.grid = np.rot90(self.grid, 1)
 
-    def move_down(self, test_move):
+    def move_down(self, test_move=False):
         """
             Will rotate the board matrice twice to the left, thus making it like a down move is an up move. We can thus
             apply move_up and then rotate backward the matrice.
--- remastered/tile.py
+++ remastered/tile.py
@@ -9,7 +9,7 @@ class Tile(pygame.sprite.Sprite):
 
         self.value = value
         self.posX, self.posY = pos
-        self.targetPosX, self.targetPosY = pos
+
         # creating the surface tile
         self.image = pygame.Surface((TILESIZE, TILESIZE))
         self.rect = self.image.get_rect(topleft=pos)
@@ -25,37 +25,3 @@ class Tile(pygame.sprite.Sprite):
 
             # adding the text to the surface
             self.image.blit(self.text_surf, self.text_rect)
-
-        self.direction = pygame.math.Vector2()
-        self.speed = SPEED
-
-    def move(self, speed):
-        self.rect.center += self.direction * speed
-        self.posX += self.direction[0] * speed
-        self.posY += self.direction[1] * speed
-
-    def set_direction(self, direction):
-        self.direction = direction
-
-    def finished_movement(self):
-        """
-            Tell if the movement is finished or not. 
-        """
-        if self.posX == self.targetPosX and self.posY == self.targetPosY:
-            self.set_direction(pygame.math.Vector2())
-        return self.posX == self.targetPosX and self.posY == self.targetPosY
-
-    def update_value(self, value):
-        self.value = value
-        self.image.fill(COLORS[self.value])
-        self.text_surf = self.font.render(f'{self.value}', True, 'Black')
-        self.text_rect = self.text_surf.get_rect(center=(TILESIZE / 2, TILESIZE / 2))
-        self.image.blit(self.text_surf, self.text_rect)
-
-    def new_target(self, direction):
-        self.targetPosX = self.targetPosX + TILEDISPLACEMENT * direction[0]
-        self.targetPosY = self.targetPosY + TILEDISPLACEMENT * direction[1]
-
-    def update(self):
-        self.move(self.speed)
-        debug(str(self.posX) + " " + str(self.targetPosX))
